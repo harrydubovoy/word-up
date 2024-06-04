641bc40799380969da027f98ec1196fd41852c02
harrydubovoy <harrydubovoy@gmail.com>
2024-06-04 15:37:35 +0300

    add drawer for description

--- src/screen/ListScreen/index.jsx
+++ src/screen/ListScreen/index.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { map, prop, reverse, has, compose, length, equals } from 'ramda';
+import { map, prop, reverse, has, compose, length } from 'ramda';
 
-import classNames from 'classnames';
 import { IconButton } from '../../ui/IconButton';
+import { Drawer } from '../../ui/Drawer';
 import { Input } from '../../ui/Input';
 import { Typography } from '../../ui/Typography';
 import { Menu, MenuItem, MenuHandler, MenuList } from '../../ui/Menu';
 import { Button } from '../../ui/Button';
 import { Navbar } from '../../ui/Navbar';
-
-import { FlipCard } from '../../components/FlipCard';
 
 import ScrollContainer from '../../screen-components/ScrollContainer';
 import ScreenBody from '../../screen-components/ScreenBody';
@@ -24,6 +22,7 @@ import PublicSvg from '../../icons/PublicSvg';
 import EditSvg from '../../icons/EditSvg';
 import ListAdd from '../../icons/ListAdd';
 import ListRemove from '../../icons/ListRemove';
+import DescriptionSvg from '../../icons/DescriptionSvg';
 
 import useSearchQuery from '../../hooks/useSearchQuery';
 import useFilterType from '../../hooks/useFilterType';
@@ -61,6 +60,9 @@ import { EMPTY_SCREEN_TYPE } from '../../constants/screens';
 function ListScreen() {
   const navigate = useNavigate();
 
+  const drawerRef = useRef(null);
+  const [drawerDescriptionId, setDrawerDescriptionId] = useState(-1);
+
   const dispatch = useAppDispatch();
   const entitiesDictionary = useAppSelector(selectEntitiesDictionary);
   const entitiesTestPlan = useAppSelector(selectEntitiesTestPlan);
@@ -68,39 +70,31 @@ function ListScreen() {
   const totalDictionary = useAppSelector(selectTotalDictionary);
   const idsTestPlan = useAppSelector(selectIdsTestPlan);
 
-  const [isActiveById, setIsActiveById] = useState(0);
-
-  const handleToggleActiveById = (id) => () => {
-    setIsActiveById((prevId) => (equals(prevId, id) ? 0 : id));
-  };
-
   const { searchString, handleOnSearchChange } = useSearchQuery();
   const { filterValue, filterDisplayValue, handleFilterTypeChange } = useFilterType();
 
-  const handleOnAddToTestPlan = (wordPairId) => (event) => {
-    event.stopPropagation();
+  const handleOpenDescriptionDrawer = (id) => () => setDrawerDescriptionId(id);
+  const handleCloseDescriptionDrawer = () => setDrawerDescriptionId(-1);
+
+  const handleOnAddToTestPlan = (wordPairId) => () => {
     dispatch(addOneTestPlan({ id: wordPairId }));
   };
 
-  const handleOnRemoveFromTestPlan = (wordPairId) => (event) => {
-    event.stopPropagation();
+  const handleOnRemoveFromTestPlan = (wordPairId) => () => {
     dispatch(removeOneTestPlan(wordPairId));
   };
 
-  const handleOnRemove = (wordPairId) => (event) => {
-    event.stopPropagation();
+  const handleOnRemove = (wordPairId) => () => {
     dispatch(addOneTrashBin(prop(wordPairId)(entitiesDictionary)));
     dispatch(removeOneDictionary(wordPairId));
     dispatch(removeOneTestPlan(wordPairId));
   };
 
-  const handleOnEdit = (wordPairId) => (event) => {
-    event.stopPropagation();
+  const handleOnEdit = (wordPairId) => () => {
     navigate(`/edit/${wordPairId}`);
   };
 
-  const handleOnOpenDictionary = (wordPairId) => (event) => {
-    event.stopPropagation();
+  const handleOnOpenDictionary = (wordPairId) => () => {
     openExternalDictionaryPageByWord(getForeignWordById(wordPairId)(entitiesDictionary));
   };
 
@@ -113,6 +107,17 @@ function ListScreen() {
   return (
     <>
       <Header />
+      <Drawer
+        className="absolute rounded-t-3xl p-6"
+        placement="bottom"
+        overlayRef={drawerRef}
+        open={Boolean(drawerDescriptionId)}
+        onClose={handleCloseDescriptionDrawer}
+      >
+        <Typography variant="paragraph" color="blue-gray" className="text-left">
+          {getDescriptionWordById(drawerDescriptionId)(entitiesDictionary)}
+        </Typography>
+      </Drawer>
       <ScrollContainer>
         <Navbar className="rounded-t-none sticky top-0 z-10 p-4">
           <div className="flex items-center justify-between gap-3">
@@ -139,66 +144,48 @@ function ListScreen() {
         </Navbar>
         <EmptyScreen type={!length(filteredIdsDictionary) && EMPTY_SCREEN_TYPE.DEFAULT}>
           <ScreenBody className="bg-catskill-white">
-            <div className="w-full">
+            <div ref={drawerRef} className="w-full">
               <div className="grid grid-cols-1 gap-4">
                 {map((wordPairId) => (
-                  <FlipCard
-                    className={classNames('h-36', {
-                      'cursor-pointer': getDescriptionWordById(wordPairId)(entitiesDictionary) })}
-                    isToggled={
-                    getDescriptionWordById(wordPairId)(entitiesDictionary)
-                      && equals(isActiveById, wordPairId)
-                    }
-                    key={wordPairId}
-                    renderFront={({ frontClassName }) => (
-                      <WordPairCard
-                        onClick={handleToggleActiveById(wordPairId)}
-                        className={frontClassName}
+                  <WordPairCard key={wordPairId}>
+                    <WordPairCard.Body>
+                      <WordPair
+                        isSelected={has(wordPairId)(entitiesTestPlan)}
+                        foreign={getForeignWordById(wordPairId)(entitiesDictionary)}
+                        native={getNativeWordById(wordPairId)(entitiesDictionary)}
+                        transcription={getTranscriptionWordById(wordPairId)(entitiesDictionary)}
+                        partOfSpeech={getPartOfSpeechWordById(wordPairId)(entitiesDictionary)}
+                      />
+                    </WordPairCard.Body>
+                    <WordPairCard.Footer>
+                      <IconButton variant="outlined" size="sm" onClick={handleOnRemove(wordPairId)}>
+                        <TrashSvg />
+                      </IconButton>
+                      <IconButton variant="outlined" size="sm" onClick={handleOnOpenDictionary(wordPairId)}>
+                        <PublicSvg />
+                      </IconButton>
+                      <IconButton variant="outlined" size="sm" onClick={handleOnEdit(wordPairId)}>
+                        <EditSvg />
+                      </IconButton>
+                      <IconButton
+                        disabled={!getDescriptionWordById(wordPairId)(entitiesDictionary)}
+                        variant="outlined"
+                        size="sm"
+                        onClick={handleOpenDescriptionDrawer(wordPairId)}
                       >
-                        <WordPairCard.Body>
-                          <WordPair
-                            isSelected={has(wordPairId)(entitiesTestPlan)}
-                            foreign={getForeignWordById(wordPairId)(entitiesDictionary)}
-                            native={getNativeWordById(wordPairId)(entitiesDictionary)}
-                            transcription={getTranscriptionWordById(wordPairId)(entitiesDictionary)}
-                            partOfSpeech={getPartOfSpeechWordById(wordPairId)(entitiesDictionary)}
-                          />
-                        </WordPairCard.Body>
-                        <WordPairCard.Footer>
-                          <IconButton variant="outlined" size="sm" onClick={handleOnRemove(wordPairId)}>
-                            <TrashSvg />
-                          </IconButton>
-                          <IconButton variant="outlined" size="sm" onClick={handleOnOpenDictionary(wordPairId)}>
-                            <PublicSvg />
-                          </IconButton>
-                          <IconButton variant="outlined" size="sm" onClick={handleOnEdit(wordPairId)}>
-                            <EditSvg />
-                          </IconButton>
-                          {entitiesTestPlan[wordPairId] ? (
-                            <IconButton size="sm" onClick={handleOnRemoveFromTestPlan(wordPairId)}>
-                              <ListRemove />
-                            </IconButton>
-                          ) : (
-                            <IconButton size="sm" onClick={handleOnAddToTestPlan(wordPairId)}>
-                              <ListAdd />
-                            </IconButton>
-                          )}
-                        </WordPairCard.Footer>
-                      </WordPairCard>
-                    )}
-                    renderBack={({ backClassName }) => (
-                      <WordPairCard
-                        onClick={handleToggleActiveById(wordPairId)}
-                        className={backClassName}
-                      >
-                        <WordPairCard.Body>
-                          <Typography variant="paragraph" color="blue-gray" className="text-left">
-                            {getDescriptionWordById(wordPairId)(entitiesDictionary)}
-                          </Typography>
-                        </WordPairCard.Body>
-                      </WordPairCard>
-                    )}
-                  />
+                        <DescriptionSvg />
+                      </IconButton>
+                      {entitiesTestPlan[wordPairId] ? (
+                        <IconButton size="sm" onClick={handleOnRemoveFromTestPlan(wordPairId)}>
+                          <ListRemove />
+                        </IconButton>
+                      ) : (
+                        <IconButton size="sm" onClick={handleOnAddToTestPlan(wordPairId)}>
+                          <ListAdd />
+                        </IconButton>
+                      )}
+                    </WordPairCard.Footer>
+                  </WordPairCard>
                 ), filteredIdsDictionary)}
               </div>
             </div>
---
+++ src/ui/Drawer.jsx
@@ -0,0 +1,3 @@
+import { Drawer as DrawerCore } from '@material-tailwind/react';
+
+export const Drawer = DrawerCore;
