66b7bf349d564754c564983394aa2ada73d47be7
harrydubovoy <harrydubovoy@gmail.com>
2024-06-10 09:50:35 +0300

    fix available part of speech

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,4 +1,4 @@
-import { compose, isEmpty, pluck, reject, uniq, values } from 'ramda';
+import { compose, pluck, reject, uniq, values, either, isNil, isEmpty } from 'ramda';
 
 export const generateId = () => Number(new Date()) + Math.floor(Math.random() * 100);
 
@@ -6,7 +6,7 @@ export const reverseValue = (value) => !value;
 
 export const getUniqueValuesByField = (field) => compose(
   uniq,
-  reject(isEmpty),
+  reject(either(isEmpty, isNil)),
   pluck(field),
   values,
 );
