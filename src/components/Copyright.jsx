17b311c155ee213cc5930a5ce8a9490ca74f0201
harrydubovoy <harrydubovoy@gmail.com>
2024-06-15 16:08:42 +0300

    change copyright color text

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -6,12 +6,12 @@ function Copyright() {
   return (
     <Typography
       variant="small"
-      className="text-center block"
+      className="text-center block text-slate-400"
     >
       &copy;&nbsp;2023&nbsp;&mdash;&nbsp;
       {getYear()}
       &nbsp;WordUp by&nbsp;
-      <a className="hover:text-slate-400" href="https://github.com/harrydubovoy" target="_blank">@harrydubovyi</a>
+      <a className="hover:text-slate-600" href="https://github.com/harrydubovoy" target="_blank">@harrydubovyi</a>
     </Typography>
   );
 }
