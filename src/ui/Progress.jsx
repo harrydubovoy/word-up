c7dd3bbf5b4656fe15973fb2752b83cffdde6aa4
harrydubovyi <harrydubovoy@gmail.com>
2024-11-11 11:11:17 +0200

    fix progress color

--- src/ui/Progress.jsx
+++ src/ui/Progress.jsx
@@ -3,8 +3,8 @@ import * as React from 'react';
 import { cn } from '../lib/utils';
 
 const Progress = React.forwardRef(({ className, value, ...props }, ref) => (
-  <div {...props} ref={ref} className={cn('w-full h-2', 'rounded-full', 'bg-primary/20', className)}>
-    <div className={cn('bg-primary', 'h-2', 'rounded-full')} style={{ width: `${value}%` }} />
+  <div {...props} ref={ref} className={cn('w-full h-2', 'rounded-full', 'bg-neutral/20', className)}>
+    <div className={cn('bg-brand', 'h-2', 'rounded-full')} style={{ width: `${value}%` }} />
   </div>
 ));
 Progress.displayName = 'Progress';
